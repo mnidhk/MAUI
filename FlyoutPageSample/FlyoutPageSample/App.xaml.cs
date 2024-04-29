3161b39d64d32dc7dfd16c0c13acaf4a44318f19
Michel van Nistelrooij <michel.vannistelrooij@axians.com>
2024-04-29 11:48:06 +0200

    FlyoutPageSample - Bug Icon image not show in navigatiepagina

---
+++ FlyoutPageSample/FlyoutPageSample/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace FlyoutPageSample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+        //NOT Working, does not show Hamburger icon
+        MainPage = new NavigationPage( new AppFlyout());
+
+		//Working, shows Hamburger icon
+        //MainPage = new AppFlyout();
+    }
+}
